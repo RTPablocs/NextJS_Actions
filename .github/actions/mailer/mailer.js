3a6cd97710034258c095473cea4a9d52036ef795
RTPablocs <conejerosorianopablo@gmail.com>
2021-12-19 20:27:34 +0100

    Mailer test #1

---
+++ .github/actions/mailer/mailer.js
@@ -0,0 +1,26 @@
+const mailer = require('nodemailer')
+const core = require('@actions/core')
+
+
+const transporter = mailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: core.getInput('mail_sender'),
+        pass: core.getInput('mail_password')
+    }
+});
+
+const mailOptions = {
+    from: core.getInput('mail_sender'),
+    to: core.getInput('mail_reciever'),
+    subject: 'Resultado de la action de NextJS',
+    text: 'This is a testt'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
